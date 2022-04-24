193d494ff421bfb440a2b506ce9dda0a5546ce4a
Arinjaychaudhary <76354318+Arinjaychaudhary@users.noreply.github.com>
2022-04-24 10:49:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,13 +23,18 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent("canvas");
+  video=createCapture(VIDEO);
+ video.size(700,600);
+ video.hide()
+ poseNet=ml5.poseNet(video,modelLoaded);
 }
 
 
 function draw(){
 
  background(0); 
-
+image(video,0,0,700,600);
  fill("black");
  stroke("black");
  rect(680,0,20,700);
@@ -67,7 +72,9 @@ function draw(){
     move();
 }
 
-
+function modelLoaded(){
+  console.log("Model Loaded Successfully");
+}
 
 //function reset when ball does notcame in the contact of padde
 function reset(){
